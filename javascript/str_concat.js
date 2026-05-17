ecb1c04c52b04a484c411228a0e1522a8e5a59d2
Oghenetega Godwin <107396301+oghenetega-godwin@users.noreply.github.com>
2026-05-17 05:15:57 +0100

    Delete javascript directory

--- javascript/str_concat.js
+++
@@ -1,9 +0,0 @@
-//string concat using (+), (+=) and concat() method 
-
-// using (+) 
-let state = "Delta";
-let capital = "Asaba";
-
-let state_and_capital = state + " " + capital;
-console.log(state_and_capital)
-
